6a1ae96f2cc40dbc5103ab0a786acdc9ee378c00
franck <contact@open-time.net>
2026-05-14 17:04:02 +0200

    JS code review

--- js/mymeta.js
+++ js/mymeta.js
@@ -1,8 +1,8 @@
-/*global $, dotclear, metaEditor */
+/*global $, dotclear */
 'use strict';
 
 dotclear.ready(() => {
   const mymeta = dotclear.getData('mymeta');
   dotclear.msg.confirm_tag_delete = mymeta.msg;
-  $('#tag_delete').on('submit', () => window.confirm(dotclear.msg.confirm_tag_delete));
+  $('#tag_delete').on('submit', () => globalThis.confirm(dotclear.msg.confirm_tag_delete));
 });
